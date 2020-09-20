ed6fc9b8688983b47d677533b1e5707ad9a96f61
Mithunmick <fazle.mithun@northsouth.edu>
2020-09-20 15:55:27 +0600

    Completed Navbar & header

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,16 +1,67 @@
-import React from 'react'
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Typed from 'react-typed';
+
 import {
     Typography,
     Avatar,
     Grid,
-    Box
+    Box,
 } from '@material-ui/core';
 import avatar from "../Components/IMG/Avater.png";
 
+
+// CSS styles
+const useStyles = makeStyles(theme=> ({
+    avatar: {
+        width: theme.spacing(20),
+        height: theme.spacing(30),
+        margin: theme.spacing(1),
+        
+        zIndex: "-1"
+
+    },
+    title: {
+        color: "tomato"
+    },
+    subtitle: {
+        color: "tan"
+    },
+    typeContainer: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100vw",
+        textAlign: "center",
+        zIndex: 1
+    },
+    
+    
+}))
+
 const Header = () => {
+    const classes = useStyles()
     return (
-        <Box>
-            <Avatar src={avatar} alt="Mithunm" />
+        
+        
+        <Box className={classes.typeContainer}>
+            <Grid container justify="center" >
+            <Avatar className={classes.avatar} src={avatar} alt="Mithunm" />
+            
+            </Grid>
+            <Typography className={classes.title} variant="h4" >
+                <Typed strings={["Fazle Rabbi Mithun"]} TypeSpeed={40} />
+            </Typography>
+            <br/>
+            <Typography className={classes.subtitle} variant="h5" >
+                <Typed
+                    strings={["Web Design", "Web Development","Full Stack"]}
+                    TypeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+            </Typography>
         </Box>
     );
 };
--- portfolio/src/Components/index.js
+++ portfolio/src/Components/index.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import Header from './Header';
 import Navbar from './Navbar';
+import Particles from "react-particles-js";
+import {makeStyles} from "@material-ui/styles";
+
+
+
+const useStyles = makeStyles ({
+    particlesCanva: {
+        position: "absolute"
+    }
+})
 
 
 const Home = () => {
+
+    const classes = useStyles()
+
     return (
-        <div>
+        <>
             <Navbar />
             <Header />
-        </div>
+            <Particles 
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number: {
+                            value: 50,
+                            density: {
+                                enable: true,
+                                value_area: 1000
+                            }
+                        },
+                        shape: {
+                            type: "circle",
+                            stroke : {
+                                width: 1,
+                                color: "tomato"
+                            }
+                        },
+                        size: {
+                            value: 5,
+                            random: true,
+                            anim: {
+                                enable: false,
+                                speed: 5,
+                                size_min: .1,
+                                sync: true
+                            }
+                        },
+                        opacity: {
+                            value: 1,
+                            random: true,
+                            anim: {
+                                enable: true,
+                                speed: 1,
+                                opacity_min:.1,
+                                sync: true
+                            }
+                        }
+                    }
+                }}
+            />
+            
+        </>
     );
 };
 
